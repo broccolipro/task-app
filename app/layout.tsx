fb1955d71b2564ade78189c983ea116800571a91
broccolipro <mecloud.manage@gmail.com>
2024-11-15 23:58:25 +0200

    ant removed 1.0

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
-import {AntdRegistry} from "@ant-design/nextjs-registry";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -29,7 +28,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased bg-white`}
       >
-      <AntdRegistry>{children}</AntdRegistry>
+{children}
       </body>
     </html>
   );
